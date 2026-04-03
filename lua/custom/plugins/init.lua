6409237821ec90cfefba464ffff95e66c930aeae
dkstile4 <davidstiles62@yahoo.com>
2026-04-02 20:43:24 -0400

    feat: Terminal Split on the bottom pane
    Terminal Split on the bottom pane

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Open terminal in bottom split on startup
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    -- Split window and open terminal
+    vim.cmd('split')
+    vim.cmd('terminal')
+    vim.cmd('wincmd j') -- Move to the new terminal window
+  end,
+  once = true,
+})
+
 return {}
